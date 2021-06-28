b4f70624172d3d8ef832245f75f7b969d32d1ae7
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-06-28 11:35:40 -0500

    Desarrollo funcionalidad de SUMA

--- dom-intro/index.js
+++ dom-intro/index.js
@@ -1,32 +1,38 @@
-console.log("Hola Mundo")
-
-
 // 1. VARIABLES
-
-const nombre = document.getElementById("capturar-nombre")
-const btnSaludar = document.getElementById("btn-saludar")
-const areaSaludo = document.getElementById("area-saludo")
+const nombre        = document.getElementById("capturar-nombre")
+const btnSaludar    = document.getElementById("btn-saludar")
+const areaSaludo    = document.getElementById("area-saludo")
+const valorA        = document.getElementById("valor-a")
+const valorB        = document.getElementById("valor-b")
+const btnSumar      = document.getElementById("btn-sumar")
+const areaSuma      = document.getElementById("area-suma")
 
 
 // 2. FUNCIONES
-
 const saludar = (name) => {
 
-    const saludo = `Hola ${name} !!!`
-
+    const saludo = `Hola ${name}!!!`
+    
     return areaSaludo.innerHTML = saludo
+}
+const sumar = (a, b) => {
 
+    const resultado = a + b
+
+    return areaSuma.innerHTML = resultado
 }
 
 
+
 // 3. EVENTOS
-
 btnSaludar.addEventListener("click", () => {
-    
     const nameValue = nombre.value
+    return saludar(nameValue)
+})
+btnSumar.addEventListener("click", () => {
 
-    console.log(nameValue)
+    const inputValorA   = parseInt(valorA.value)
+    const inputValorB   = parseInt(valorB.value)
 
-    saludar(nameValue)
-
+    return sumar(inputValorA, inputValorB)
 })

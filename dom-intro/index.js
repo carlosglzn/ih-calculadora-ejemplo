5ed6064e1107b905c9a484c0f367e294d793e975
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-06-28 12:02:19 -0500

    Desarrollo de función RESTA

--- dom-intro/index.js
+++ dom-intro/index.js
@@ -4,8 +4,12 @@ const btnSaludar    = document.getElementById("btn-saludar")
 const areaSaludo    = document.getElementById("area-saludo")
 const valorA        = document.getElementById("valor-a")
 const valorB        = document.getElementById("valor-b")
+const valorAResta   = document.getElementById("valor-a-resta")
+const valorBResta   = document.getElementById("valor-b-resta")
 const btnSumar      = document.getElementById("btn-sumar")
+const btnRestar     = document.getElementById("btn-restar")
 const areaSuma      = document.getElementById("area-suma")
+const areaResta     = document.getElementById("area-resta")
 
 
 // 2. FUNCIONES
@@ -15,24 +19,56 @@ const saludar = (name) => {
     
     return areaSaludo.innerHTML = saludo
 }
+
+/**
+ * 
+ * DENTRO DE LA PROGRAMACIÓN FUNCIONAL, EXISTE UN CONCEPTO LLAMADO 
+ * PUREZA.
+ * LA PUREZA DE LAS FUNCIONES TE DICE QUE DEBEN SER LO MÁS INDEPENDIENTES
+ * POSIBLES, SUS ÚNICOS ACCESOS ESTÁN DENTRO DE LA MISMA FUNCIÓN
+ * Y A TRAVÉS DE SUS PARÁMETROS. NO MÁS.
+ */
+
+
 const sumar = (a, b) => {
 
-    const resultado = a + b
+    return a + b
 
-    return areaSuma.innerHTML = resultado
+}
+
+const restar = (a, b) => {
+
+    return a - b
+
 }
 
 
 
 // 3. EVENTOS
 btnSaludar.addEventListener("click", () => {
+
     const nameValue = nombre.value
-    return saludar(nameValue)
+    
+    saludar(nameValue)
+
 })
+
 btnSumar.addEventListener("click", () => {
 
     const inputValorA   = parseInt(valorA.value)
     const inputValorB   = parseInt(valorB.value)
 
-    return sumar(inputValorA, inputValorB)
+    const resultado = sumar(inputValorA, inputValorB)
+
+    areaSuma.innerHTML = resultado
+})
+
+btnRestar.addEventListener("click", () => {
+
+    const inputValorAResta = parseInt(valorAResta.value)
+    const inputValorBResta = parseInt(valorBResta.value)
+
+    const resultadoResta = restar(inputValorAResta, inputValorBResta)
+
+    areaResta.innerHTML = resultadoResta
 })
